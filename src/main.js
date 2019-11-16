a81b16830c5b916aa1caf01dd7aa74081a1fb603
Vincent <vincentmuijtjens@ziggo.nl>
2019-11-16 16:10:54 +0100

    Added router on a different way. In an attempt to fix the routing

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import VueSession from 'vue-session'
-import router from './plugins/router'
+import routes from './plugins/router'
 
 
 Vue.config.productionTip = false;
@@ -15,7 +15,7 @@ Vue.use(VueSession, options);
 
 new Vue({
   vuetify,
+  router: routes,
   VueSession,
-  router,
   render: h => h(App)
 }).$mount('#app')
