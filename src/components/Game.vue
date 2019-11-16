b44204cb882513f5fdde86afa868ef7eef0d04df
Vincent <vincentmuijtjens@ziggo.nl>
2019-11-16 15:27:28 +0100

    Fixed the issue that when a new lobby is created, it automatically updates it

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,28 +1,30 @@
 <template>
-    <v-container>
         <v-container id="lobbyList">
             <h1 style="padding: 12px">Lobby list</h1>
-        </v-container>
-        <!-- Players are loaded in here -->
-            <v-row id="container">
-            </v-row>
+            <LobbyInfo
+            v-for="lobby in this.lobbyList"
+            v-bind:key="lobby"
+            v-bind:lobby-id="lobby"
+            v-bind:websocket="websocket"
+            />
         </v-container>
 </template>
 
 <script>
-import Vue from 'vue'
-import LobbyPlayer from "./LobbyPlayer";
-import Lobby from "./Lobby";
+import LobbyInfo from "@/components/LobbyInfo";
 
     export default {
         name: "Game",
+        components: {LobbyInfo},
         data: () => ({
-            websocket: null
+            websocket: null,
+            lobbyList: []
         }),
         methods: {
         },
         created(){
-            this.websocket = new WebSocket("ws://localhost:8090/ws/lobby/" + this.$session.get('jwttoken'));
+            this.websocket= new WebSocket("ws://localhost:8090/ws/lobby/" + this.$session.get('jwttoken'));
+            var component = this;
             this.websocket.onmessage = function(message)
             {
                 if(message.data === "You successfully connected")
@@ -41,50 +43,25 @@ import Lobby from "./Lobby";
                         let data = `{"task": "JoinGame", "nickname": "John", "gameSessionId": "${gameSessionId}"}`;
                         this.send(data);
                         break;
-                    case 'addPlayers':
-                        for(let i = 0; i < json['players'].length; i++)
-                        {
-                            // eslint-disable-next-line no-case-declarations
-                            let ComponentClass = Vue.extend(LobbyPlayer);
-                            let instance = new ComponentClass({
-                                propsData: { playerName: json['players'][i] }
-                            });
-                            instance.$mount();
-                            document.getElementById('container').append(instance.$el);
-                        }
-                        break;
-                    case 'addNewPlayer':
-                        // eslint-disable-next-line no-case-declarations
-                        let ComponentClass = Vue.extend(LobbyPlayer);
-                        // eslint-disable-next-line no-case-declarations
-                        let instance = new ComponentClass({
-                            propsData: { playerName: json['newPlayer'] }
-                        });
-                        instance.$mount();
-                        document.getElementById('container').append(instance.$el);
-                        break;
-                    case 'removePlayer':
-                        document.getElementById(json['removedPlayer']).remove();
-                        break;
                     case 'updateGameList':
                         for(let i = 0; i < json['gameLobbys'].length; i++)
                         {
-                            // eslint-disable-next-line no-case-declarations
-                            let ComponentClass = Vue.extend(Lobby);
-                            // eslint-disable-next-line no-case-declarations
-                            let instance = new ComponentClass({
-                                propsData: { lobbyId: json['gameLobbys'][i], webSocket: this }
-                            });
-                            instance.$mount();
-                            document.getElementById('lobbyList').append(instance.$el);
+                            component.lobbyList.push(json['gameLobbys'][i]);
                         }
                         break;
                 }
             };
         },
         beforeRouteLeave(to, from, next) {
-            this.websocket.close();
-            next();
+            if(to.path === "/main/lobby")
+            {
+                next();
+            }
+            else
+            {
+                this.websocket.close();
+                next();
+            }
         }
     }
 </script>
