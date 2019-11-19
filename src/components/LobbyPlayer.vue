021730c058a4ac4971e12ccc6fb1cce9d81996ae
BlackMania <BlackMania@users.noreply.github.com>
2019-11-19 16:53:18 +0100

    Added new div with roommaster

--- src/components/LobbyPlayer.vue
+++ src/components/LobbyPlayer.vue
@@ -25,6 +25,8 @@
                 <v-card-title>
                     {{ playerName }}
                 </v-card-title>
+                <div id="roomMaster">
+                </div>
             </v-card-actions>
         </v-card>
     </v-col>
@@ -34,7 +36,8 @@
     export default {
         name: "LobbyPlayer",
         props: {
-            playerName: String
+            playerName: String,
+            roomMaster: String
         },
         data () {
             return {
@@ -45,6 +48,22 @@
                 timeout: 6000,
                 x: null,
                 y: 'top',
+            }
+        },
+        watch: {
+            roomMaster: function() {
+                if(this.playerName === this.roomMaster)
+                {
+                    let element = document.getElementById('roomMaster');
+                    element.innerHTML = "Roommaster";
+                }
+            }
+        },
+        mounted() {
+            if(this.playerName === this.roomMaster)
+            {
+                let element = document.getElementById('roomMaster');
+                element.innerHTML = "Roommaster";
             }
         }
     }
